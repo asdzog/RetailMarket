5eb3292ebde137c1ad3c9c2dfa0910c233115da3
asd <asdzog@mail.ru>
2024-05-13 03:28:24 +0400

    add views for app retailers

--- retailers/views.py
+++ retailers/views.py
@@ -1,21 +1,56 @@
-from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
+from retailers.models import Product, Contact, NetworkNode
 from retailers.serializers import ProductSerializer, ContactSerializer, NetworkNodeSerializer
+from users.permissions import IsActiveModer
 
 
-# Create your views here.
 class ProductCreateAPIView(generics.CreateAPIView):
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsActiveModer]
+
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsActiveModer]
 
 
 class ContactCreateAPIView(generics.CreateAPIView):
     serializer_class = ContactSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsActiveModer]
+
+
+class ContactListAPIView(generics.ListAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    permission_classes = [IsAuthenticated, IsActiveModer]
 
 
 class NetworkNodeCreateAPIView(generics.CreateAPIView):
     serializer_class = NetworkNodeSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsActiveModer]
+
+
+class NetworkNodeRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = NetworkNodeSerializer
+    queryset = NetworkNode.objects.all()
+    permission_classes = [IsAuthenticated, IsActiveModer]
+
+
+class NetworkNodeUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = NetworkNodeSerializer
+    queryset = NetworkNode.objects.all()
+    permission_classes = [IsAuthenticated, IsActiveModer]
+
+
+class NetworkNodeDeleteAPIView(generics.DestroyAPIView):
+    queryset = NetworkNode.objects.all()
+    permission_classes = [IsAuthenticated, IsActiveModer]
+
+
+class NetworkNodeListAPIView(generics.ListAPIView):
+    serializer_class = NetworkNodeSerializer
+    queryset = NetworkNode.objects.all()
+    permission_classes = [IsAuthenticated, IsActiveModer]
