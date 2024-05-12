b4329645cda08312cb69fb043514cc7584dd89b6
asd <asdzog@mail.ru>
2024-05-13 03:28:44 +0400

    add urls for app retailers

--- retailers/urls.py
+++ retailers/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path, include
+from django.urls import path
 from retailers.apps import RetailersConfig
-from retailers.views import NetworkNodeCreateAPIView
+from retailers.views import NetworkNodeCreateAPIView, ContactListAPIView, ProductListAPIView, NetworkNodeListAPIView, \
+    NetworkNodeDeleteAPIView, NetworkNodeUpdateAPIView, NetworkNodeRetrieveAPIView
 
 app_name = RetailersConfig.name
 
 
 urlpatterns = [
-    path('create-retailer/', NetworkNodeCreateAPIView.as_view(), name='create-retailer'),
+    path('create/', NetworkNodeCreateAPIView.as_view(), name='create'),
+    path('<int:pk>/details/', NetworkNodeRetrieveAPIView.as_view(), name='details'),
+    path('<int:pk>/update/', NetworkNodeUpdateAPIView.as_view(), name='update'),
+    path('<int:pk>/delete/', NetworkNodeDeleteAPIView.as_view(), name='delete'),
+    path('', NetworkNodeListAPIView.as_view(), name='list'),
+    path('contacts/', ContactListAPIView.as_view(), name='contacts'),
+    path('products/', ProductListAPIView.as_view(), name='products'),
 ]
